76fe8c615717c063cad2016f0f95d3cee4ff6736
Shama <shamaanjum09@outlook.com>
2018-02-12 13:45:10 -0800

    twitter passport login initial

---
+++ config/auth.js
@@ -0,0 +1,11 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+    'twitterAuth' : {
+        'consumerKey'        : 'VDPgSyJ8vMjSn0PjAZBtQgdPO',
+        'consumerSecret'     : '94h1wLHTXPIc3btcZQoBiofmzNbUs2nrUG9c3vUOPsTFcUv3BC',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    }
+};
